81f4b0d53c34c5653931df4352c23afb908538a3
JLITE <jlite_tool>
2024-08-03 16:35:28 +0800

    update

--- benchmarks/process.sh
+++ benchmarks/process.sh
@@ -1,3 +1,4 @@
+rm -rf ../logs
 mkdir -p ../logs
 cp -r dacapo/`cat dacapo/log_dir.txt` ../logs/dacapo
 cp -r renaissance/`cat renaissance/log_dir.txt` ../logs/renaissance
@@ -5,6 +6,6 @@ cp -r spec/SPECjvm2008/`cat spec/SPECjvm2008/log_dir.txt` ../logs/spec
 cd ../scripts
 ./process_log.sh
 
-./overhead.py
-./overhead.py 2
+python3 ./overhead.py
+python3 ./overhead.py 2
 mv *.pdf ../benchmarks
